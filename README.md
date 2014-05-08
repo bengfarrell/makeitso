46bcf3fdeb1fff4aab64fbf5f60cfea16ba798e9
ben <ben@benfarrell.com>
2014-05-07 23:33:02 -0700

    added command callbacks

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 makeitso
 ========
+
+Heavily stolen from https://github.com/sreuter/node-speakable and then torn apart and reassembled.
+
+Help from https://github.com/gillesdemey/google-speech-v2 to get the broken API back up and running
+
+And implemented ATT speech from https://github.com/mko/watson-js when Google's API broke on me
--- example.js
+++ example.js
@@ -3,12 +3,14 @@ var MakeItSo = require('./lib/makeitso');
 // Setup google speech
 var opts = {
     soxLocation: "C:/Program Files (x86)/sox-14-4-1/sox",
-    verbose: true,
-    silenceTrimThresholdStart: '0.1%',
-    silenceTrimThresholdEnd: '0.1%',
+    verbose: false,
+    continuous: true,
+    silenceTrimThresholdStart: '5%',
+    silenceTrimThresholdEnd: '5%',
     service: "google",
     google_key: "AIzaSyCnl6MRydhw_5fLXIdASxkLJzcJh5iX0M4"
 }
+
 var makeitso = new MakeItSo(opts);
 
 makeitso.on('speechStart', function() {
@@ -23,17 +25,12 @@ makeitso.on('speechReady', function() {
     //  console.log('onSpeechReady');
 });
 
-makeitso.on('error', function(err) {
-    console.log('onError:');
-    console.log(err);
-    makeitso.start();
-});
+makeitso.on('error', function(err) {});
 
 makeitso.on('speechResult', function(results) {
     console.log('onSpeechResult:');
     console.log(results)
-    //console.log(spokenWords.isInterimResults.toString() + ": " + JSON.stringify(spokenWords.hypotheses));
-    makeitso.start();
 });
 
+makeitso.addCommand(["make it so", "do it"], function(result) { console.log(result)})
 makeitso.start();
